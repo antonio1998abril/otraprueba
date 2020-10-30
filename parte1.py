1a342a82b4fe007ca822b2dd409e1907075b9dff
unknown <crashx30@hotmail.com>
2020-10-29 22:49:00 -0600

    multi

--- parte1.py
+++ parte1.py
@@ -6,8 +6,13 @@ def beta(a,b):
     resta=a-b
     print("La suma es",resta)
 
+def zelta(a,b):
+    multi=a-b
+    print("La multiplicacion es",multi)
+
 a=int(input("Ingresa  el primer valor"))
 b=int(input("Ingresa el segundo valor"))
 
 delta(a,b)
 beta(a,b)
+zelta(a,b)
