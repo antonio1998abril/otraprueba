b003e38410a6c72864e10dbd93ca7240d7430721
[antonio1998abril] <[crashx11@hotmail.com]>
2020-10-29 22:17:24 -0600

    primera

---
+++ parte1.py
@@ -0,0 +1,13 @@
+def delta(a,b):
+    suma=a+b
+    print("La suma es",suma)
+
+def beta(a,b):
+    resta=a-b
+    print("La suma es",resta)
+
+a=int(input("Ingresa  el primer valor"))
+b=int(input("Ingresa el segundo valor"))
+
+delta(a,b)
+beta(a,b)
